cc5f966dc908e8de6f99573aeced5773c11d92d7
Auri Gabriel <ririmello.a@gmail.com>
2022-11-09 21:40:52 -0300

    fix print k

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,7 @@ impl Adaline {
         for k in 1..maxiterates {
             let mut hits: i32 = 0;
 
-            print!("It= {}", k);
+            println!("It = {}", k);
 
             for i in 0..4 {
                 y_interm = self.propaga(i);
