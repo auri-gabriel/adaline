e4ae960e182318808dc63c23d238e95c4e807f5d
Auri Gabriel <ririmello.a@gmail.com>
2022-11-11 21:21:48 -0300

    output grafico

--- src/main.rs
+++ src/main.rs
@@ -145,4 +145,22 @@ fn main() {
     adaline.cria_treinamento(and);
     adaline.treinamento(1000000);
     adaline.apresenta_resultados();
+
+    let mut x1: f64 = -1.5;
+    while x1 < 1.5 {
+        //codigo vai aqui
+        let mut x2: f64 = -1.5;
+        while x2 < 1.5 {
+            // codigo vai aqui
+            if (x1.abs() - 1.0).abs() < 0.2 && (x2.abs() - 1.0).abs() < 0.2 {
+                println!("255 0 0 ");
+            } else if adaline.propaga2(x1, x2) < 0. {
+                println!("255 255 0 ");
+            } else {
+                println!("0 255 255");
+            }
+            x2 += 0.01;
+        }
+        x1 += 0.01;
+    }
 }
