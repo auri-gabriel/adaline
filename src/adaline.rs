e38ee6ed9cb4ddd4392bf66d04bbec84a4bb8267
Auri Gabriel <ririmello.a@gmail.com>
2023-02-05 21:44:14 -0300

    translate to english

--- src/adaline.rs
+++ src/adaline.rs
@@ -1,6 +1,6 @@
 use libm::tanh;
 
-pub(crate) const GANHO: f64 = 100.0;
+pub(crate) const GAIN: f64 = 100.0;
 
 #[derive(Copy, Clone)]
 pub struct Adaline {
@@ -9,7 +9,7 @@ pub struct Adaline {
     pub(crate) t: [f64; 4],
     pub(crate) b: f64,
     pub(crate) y: f64,
-    pub(crate) soma: f64,
+    pub(crate) sum: f64,
     pub(crate) eta: f64,
 }
 
@@ -34,7 +34,7 @@ impl Adaline {
 
         let eta = 0.1;
 
-        let soma = 0.0;
+        let sum = 0.0;
 
         Self {
             w,
@@ -42,95 +42,87 @@ impl Adaline {
             t: [0., 0., 0., 0.],
             b,
             y,
-            soma,
+            sum,
             eta,
         }
     }
 
-    pub(crate) fn treinamento(&mut self, maxiterates: i32) -> () {
-        let mut y_interm: f64;
+    pub(crate) fn training(&mut self, max_iterations: i32) -> () {
+        let mut intermediate_output: f64;
 
-        for k in 1..maxiterates {
+        for k in 1..max_iterations {
             let mut hits: i32 = 0;
 
             //println!("It = {}", k);
 
             for i in 0..4 {
-                y_interm = self.propaga(i);
+                intermediate_output = self.propagate(i);
 
-                self.y = Adaline::f(y_interm);
+                self.y = Adaline::f(intermediate_output);
 
-                self.atualiza_pesos(i, y_interm);
+                self.update_weights(i, intermediate_output);
 
                 if self.y == self.t[i] {
                     hits += 1;
                 } else {
-                    self.atualiza_bias(i, y_interm);
+                    self.update_bias(i, intermediate_output);
                 }
             }
 
             if hits == 4 {
-                //println!("Aprendizado concluido com {} iterações", k);
+                //println!("Learning completed with {} iterations", k);
                 break;
             }
         }
     }
 
-    pub(crate) fn propaga(self, i: usize) -> f64 {
-        let mut soma = 0.;
+    pub(crate) fn propagate(self, i: usize) -> f64 {
+        let mut sum = 0.;
 
         for j in 0..2 {
-            soma += self.x[i][j] * self.w[j];
+            sum += self.x[i][j] * self.w[j];
         }
 
-        soma + self.b
+        sum + self.b
     }
 
-    pub(crate) fn propaga2(self, x1: f64, x2: f64) -> f64 {
-        let soma: f64;
+    pub(crate) fn propagate2(self, x1: f64, x2: f64) -> f64 {
+        let sum: f64;
 
-        soma = x1 * self.w[0] + x2 * self.w[1];
+        sum = x1 * self.w[0] + x2 * self.w[1];
 
-        soma + self.b
+        sum + self.b
     }
 
-    pub(crate) fn atualiza_pesos(&mut self, i: usize, y_res: f64) -> () {
+    pub(crate) fn update_weights(&mut self, i: usize, y_res: f64) -> () {
         for j in 0..2 {
             self.w[j] += self.eta * (self.t[i] - y_res) * self.x[i][j];
         }
     }
 
-    pub(crate) fn atualiza_bias(&mut self, i: usize, y_res: f64) -> () {
-        //b+=(t[i]-y_res)*eta;
-        self.b += (self.t[i] - y_res) * self.eta;
+    pub(crate) fn update_bias(&mut self, i: usize, expected_output: f64) -> () {
+        self.b += (self.t[i] - expected_output) * self.eta;
     }
-    /*
-    void Adaline::Apresenta_Resultados()
-    {
-        for (int l=0; l<4; l++)
-            cout << l << "-th x[0]=" << x[l][0]<<" x[1]=" << x[l][1] <<" saida=" << f(Propaga(l))<< endl;
-    }
-    */
 
-    pub(crate) fn apresenta_resultados(&mut self) -> () {
+    pub(crate) fn show_results(&mut self) -> () {
         for l in 0..4 {
             println!(
-                "{} -th x[0]= {} ; x[1]= {} ; saida = {}",
+                "{} -th x[0]= {} ; x[1]= {} ; output = {}",
                 l,
                 self.x[l][0],
                 self.x[l][1],
-                Adaline::f(self.propaga(l))
+                Adaline::f(self.propagate(l))
             );
         }
     }
 
-    pub(crate) fn cria_treinamento(&mut self, a: [f64; 4]) -> () {
+    pub(crate) fn create_training(&mut self, a: [f64; 4]) -> () {
         for i in 0..4 {
             self.t[i] = a[i];
         }
     }
 
     pub(crate) fn f(arg: f64) -> f64 {
-        tanh(arg * GANHO)
+        tanh(arg * GAIN)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ fn main() {
 
     let mut adaline = adaline::Adaline::new();
 
-    adaline.cria_treinamento(and);
-    adaline.treinamento(1000000);
-    //adaline.apresenta_resultados();
+    adaline.create_training(and);
+    adaline.training(1000000);
+    //adaline.show_results();
 
     print!("P3\n301 301\n255\n");
     let mut x1: f64 = -1.5;
@@ -18,7 +18,7 @@ fn main() {
         for _ in 0..301 {
             if (x1.abs() - 1.0).abs() < 0.2 && (x2.abs() - 1.0).abs() < 0.2 {
                 print!("255 0 0 ");
-            } else if adaline.propaga2(x1, x2) < 0. {
+            } else if adaline.propagate2(x1, x2) < 0. {
                 print!("255 255 0 ");
             } else {
                 print!("0 255 255 ");
